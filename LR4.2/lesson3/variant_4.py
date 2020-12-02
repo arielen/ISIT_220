1f6c3472b698b1cf88dcc036826e27036aa773ad
Nastya <69113994+arielen@users.noreply.github.com>
2020-12-03 00:28:06 +0700

    Update variant_4.py

--- LR4.2/lesson3/variant_4.py
+++ LR4.2/lesson3/variant_4.py
@@ -4,9 +4,9 @@
 
 # TODO - добавить исключения на кратных 400
 
-years = int(input('Введите год: '))
+year = int(input('Введите год: '))
 
-if years % 4 == 0 and years % 100 != 0:
+if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
     print('Год високосный')
 else:
     print('Год не високосный')
