88d4fc481607e457dd0472820d0d1ee79336eed3
arielen <79134475418@mail.ru>
2020-12-13 22:41:26 +0700

    reformat

--- LR4.2/lesson2/variant_8.py
+++ LR4.2/lesson2/variant_8.py
@@ -1,7 +1,9 @@
+import math as m
+
 k = 1
 summa = 0
 while k <= 8:
-    formula = 1 / k + (k + 1) ** 3
+    formula = 1 / m.factorial(k) + (k + 1) ** 3
     summa += formula
     print(f'k={k}', '%.3f' % formula, '%.3f' % summa)
     k += 1
